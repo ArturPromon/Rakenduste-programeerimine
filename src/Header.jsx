2e1693fa7db237cbf9e0f123fb55ea1c7d0d692a
ArturPromon <artur.shovkunenko@gmail.com>
2019-10-03 21:57:45 +0300

    praktikum3 ul7 on tehtud

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return(
+      <div class="header">
+        <img class="header__logo" src="./images/tlu_logo.png" />
+        <div class="header__buttons">
+        <button>Login/Signup</button>
+        <button>Cart</button>
+        </div>
+      </div>
+    )
+};
+
+export default Header;
+  
