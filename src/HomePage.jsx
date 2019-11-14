59ba0bf956ace62c5e98394ec6f1f4e198cba28b
ArturPromon <artur.shovkunenko@gmail.com>
2019-11-14 21:47:06 +0200

    lab5 ul2

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Header from "./Header.jsx";
 import ItemList from "./ItemList.jsx";
+import Checkbox from "./Checkbox.jsx";
+import PropTypes from "prop-types";
+import "./homepage.css";
 
 class HomePage extends React.PureComponent{
 
@@ -8,7 +11,8 @@ constructor(props){
     super(props);
     this.state={
       items: [],
-      sellectedCategory: "phones",
+      allCategories: ["phones", "laptops"],
+      selectedCategories: ["phones"],
     };
   }
 
@@ -33,30 +37,66 @@ fetchItems = () => {
   });
 };
 
-handleDropDown(event) {
-  console.log(event.target.value);
-  this.setState({
-    sellectedCategory: event.target.value
-  });
-}
-
-getVisibleItems = ()=> {
-  return this.state.items.filter(item=> item.category ===this.state.sellectedCategory);
+handleDropDown = (event) => {
+  if(this.isSelected(event.target.name)){
+    const clone = this.state.selectedCategories.slice();
+    const index = this.state.selectedCategories.indexOf(event.target.name);
+    clone.splice(index, 1);
+    this.setState({
+      selectedCategories: clone
+    });
+  }
+  else{
+    this.setState({
+      selectedCategories: this.state.selectedCategories.concat([event.target.name])
+    })
+  }
 };
+getVisibleItems = ()=> {
+  return this.state.items.filter(item=> this.isSelected(item.category));
+};
+
+isSelected = (name) => this.state.selectedCategories.indexOf(name) >= 0;
 
   render(){
     console.log("App state", this.state);
     return(
       <>
         <Header />
-        <select onChange ={this.handleDropDown.bind(this)}>
-          <option value="phones">Phones</option>
-          <option value="laptops">Laptops</option>
-        </select> 
+        <ItemFilters
+        allCategories={this.state.allCategories}
+        handleDropDown={this.handleDropDown}
+        isSelected={this.isSelected}
+        />
         <ItemList  items ={this.getVisibleItems()}/>
       </>
     );
   }
+}
+
+const ItemFilters = ({allCategories, handleDropDown, isSelected}) => {
+  return(
+    <div className = {"itemFilters-wrapper"}>
+      {
+        allCategories.map(categoryName => {
+          return(
+            <Checkbox
+            key={categoryName}
+            name ={categoryName}
+            onChange={handleDropDown}
+            checked = {isSelected(categoryName)}
+            />
+          );
+        })
+      }
+    </div>
+  );
+};
+
+ItemFilters.propTypes = {
+  allCategories: PropTypes.array.isReqired,
+  handleDropDown: PropTypes.func.isReqired,
+  isSelected: PropTypes.func.isReqired,
 }
 
 export default HomePage;
